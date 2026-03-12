969450f4cd9d27bdbe933bedf78403186ee04f75
sinchananr <sinchananr341@gmail.com>
2026-03-12 15:09:27 +0530

    md

---
+++ blog/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.utils.text import slugify
+from accounts.models import CustomUser
+from tinymce.models import HTMLField
+
+
+class BlogPost(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='blog_posts')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    hero_image = models.ImageField(upload_to='blog/hero/', blank=True, null=True)
+    content = HTMLField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    likes = models.ManyToManyField(CustomUser, related_name='liked_posts', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            original_slug = self.slug
+            counter = 1
+            while BlogPost.objects.filter(slug=self.slug).exclude(pk=self.pk).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
